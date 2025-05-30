3917e77b6f3bda6c0e59f57044b9ef9a34fbe3b5
4melkirti <4melkirti@gmail.com>
2025-05-30 12:23:54 -0700

    Update Screen.java

--- Screen.java
+++ Screen.java
@@ -8,17 +8,16 @@ import java.awt.Color;
 import java.awt.Dimension;
 
 public class Screen extends JPanel{
-    
 
-    public Screen(){
-       //screen constructor
-    }
+    	public Screen(){
+	       //screen constructor
+	}
 
-    public Dimension getPreferredSize(){
+    	public Dimension getPreferredSize(){
 		return new Dimension(800,600);
 	}
-    public void paintComponent(Graphics g){
+	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-        //draw things here
+        	//draw things here
 	}
 }
