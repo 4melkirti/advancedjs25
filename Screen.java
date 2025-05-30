94f3864dfe4bf5b634937f2ac476476cb8136ad8
4melkirti <4melkirti@gmail.com>
2025-05-30 12:21:47 -0700

    Create Screen.java
    blank screen template

---
+++ Screen.java
@@ -0,0 +1,24 @@
+//screen template
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+import java.awt.Color;
+
+import java.awt.Dimension;
+
+public class Screen extends JPanel{
+    
+
+    public Screen(){
+       //screen constructor
+    }
+
+    public Dimension getPreferredSize(){
+		return new Dimension(800,600);
+	}
+    public void paintComponent(Graphics g){
+		super.paintComponent(g);
+        //draw things here
+	}
+}
